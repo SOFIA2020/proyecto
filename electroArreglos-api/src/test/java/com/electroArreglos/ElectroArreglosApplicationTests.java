df2586928b58668180191ef75a0f3f5a26341d75
SOFIA2020 <sofiagarciap20@gmail.com>
2024-08-07 16:24:42 -0500

    Add files via upload

---
+++ electroArreglos-api/src/test/java/com/electroArreglos/ElectroArreglosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electroArreglos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroArreglosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
